c51195d333aba625595bf009c4b30a7d4bff353a
archana <archanag@opspl.com>
2020-01-28 13:58:43 +0530

    modification on app.js.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to git sbranch.
+          Edit <code>src/App.js</code> and save to git.
         </p>
         <a
           className="App-link"
